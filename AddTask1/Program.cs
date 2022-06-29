a0f0adc813f3f07ca14e010b5726327f7ec17e06
litovkomilada <litovkomiada@yandex.ru>
2022-06-29 14:37:04 +0300

    Complete AddTask1

---
+++ AddTask1/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 1. Написать программу, которая определяет, является ли треугольник со сторонами a, b, c равнобедренным.
+
+Console.WriteLine("Введите число, соответствующее первой стороне треугольника");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число, соответствующее второй стороне треугольника");
+int numberB = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число, соответствующее третьей стороне треугольника");
+int numberC = Convert.ToInt32(Console.ReadLine());
+
+if(numberA == numberB || numberA == numberC || numberB == numberC)
+{
+    Console.WriteLine("Треугольник равнобедренный");
+}
+else
+{
+    Console.WriteLine("Треугольник НЕ равнобедренный");
+}
