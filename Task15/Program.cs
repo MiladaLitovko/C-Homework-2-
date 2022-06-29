8783f40386048c7b73970eb19cc1faf50b60ac4b
litovkomilada <litovkomiada@yandex.ru>
2022-06-29 14:24:54 +0300

    Complete Task15

--- Task15/Program.cs
+++ Task15/Program.cs
@@ -2,14 +2,23 @@
 обозначающую день недели, и проверяет, является ли этот день выходным.*/
 
 
-Console.WriteLine("Введите число, соответсвующее дню недели");
+Console.WriteLine("Введите число, соответствующее дню недели");
 int number = Convert.ToInt32(Console.ReadLine());
+Zadacha15(number);
 
-if(number == 6 || number ==7)
+
+void Zadacha15(int number)
 {
-    Console.WriteLine("Это выходной");
-}
-else
-{
-    Console.WriteLine("Это будний день");
+    if(number == 6 || number ==7)
+    {
+        Console.WriteLine("Это выходной");
+    }
+    else if(number < 1 || number > 7)
+    {
+        Console.WriteLine("Это число не соответствует дню недели");
+    }
+    else
+    {
+        Console.WriteLine("Это будний день");
+    }
 }
